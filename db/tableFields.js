55c045deac3573077815b56765dfe51bfce63a7b
Carsten Schwede <cschwede@techfak.uni-bielefeld.de>
2020-10-02 05:05:43 +0200

    Remove SearchBuilder, add AutoFilter

--- db/tableFields.js
+++ db/tableFields.js
@@ -23,6 +23,7 @@ let tableFields = {
 		searchable:true,
 		description: "",
 		editable: true,
+		filter:true,
 		render: {
 			width: 200
 		}
@@ -30,15 +31,23 @@ let tableFields = {
 
 	"BIBTEX.title": {
 		searchable:true,
+		filter:"*",
 		render: {
 			width: 300
 		}
 	},
 	"BIBTEX.author": {
 		searchable:true,
+		filter: {
+			transform: function(data) {
+				return data.split(" and ").filter(x => !!x && x != "");
+			}
+		},
 		render: {
 			transform: function(data,type) {
 				if (!data) return "";
+				if (type !== "display") return data;
+
 				let authors = data.split(" and ").filter(x => !!x && x != "").map(author => {
 					return author.split(", ").reverse().join(" ");
 				}).map(author => {
@@ -53,6 +62,8 @@ let tableFields = {
 		}
 	},
 	"BIBTEX.year": {
+		searchable:true,
+		filter:true,
 		render: {
 			width: 300
 		}
@@ -120,6 +131,7 @@ let tableFields = {
 	"PUB.CitationsPerYear": {title:"Cites/Years"},
 	"BIBTEX.abstract": {
 		title:"Abstract",
+		filter:"*",
 		render: {
 			transform: (data,b,entry) => {
 				if (!data) return "";
@@ -131,6 +143,7 @@ let tableFields = {
 
 	"META.Description": {
 		description: "",
+		filter:"*",
 		editable: true,
 		render: {
 			width: 200
@@ -138,6 +151,8 @@ let tableFields = {
 	},
 	"META.SPECIFIC_NEW_USE_CASE": {
 		title:"Use Case",
+		filter:true,
+		searchable:true,
 		description: "",
 		editable: ["YES","NO","?"],
 		render: {
@@ -146,6 +161,8 @@ let tableFields = {
 	},
 	"META.FOCUS": {
 		description: "",
+		searchable:true,
+		filter:true,
 		editable: multiple({
 			"AmI":"Ambient Intelligence",
 			"AD":"Ambient Display",
@@ -161,6 +178,8 @@ let tableFields = {
 	},
 	"META.GOAL": {
 		description: "",
+		searchable:true,
+		filter:true,
 		editable: multiple(["BEHAVIOURAL_CHANGE","INCREASE_AWARENESS","NOTIFICATION","ASSISTED_LIVING","AID_IN_ACTIVITY","UNSTATED","OTHER"]),
 		render: {
 			width: "auto"
@@ -168,6 +187,8 @@ let tableFields = {
 	},
 	"META.TOPIC": {
 		description: "",
+		searchable:true,
+		filter:true,
 		editable: multiple(["HEALTH","ENVIRONMENT","RESOURCES","EVENTS","SOCIAL","OTHER"]),
 		render: {
 			width: "auto"
@@ -175,6 +196,8 @@ let tableFields = {
 	},
 	"META.SETTING": {
 		description: "",
+		searchable:true,
+		filter:true,
 		editable: multiple(["HOME","EDUCATION","WORKPLACE","TRANSPORT","MEDICAL","SOCIAL","OTHER"]),
 		render: {
 			width: "auto"
@@ -182,6 +205,7 @@ let tableFields = {
 	},
 	"MAPPING.HAS_MAPPING": {
 		description: "",
+		filter:true,
 		editable: ["YES","NO","?"],
 		render: {
 			width: "auto"
@@ -189,6 +213,7 @@ let tableFields = {
 	},
 	"MAPPING.FROM": {
 		description: "",
+		filter:true,
 		editable: true,
 		render: {
 			width: 200
@@ -196,6 +221,7 @@ let tableFields = {
 	},
 	"MAPPING.ORIGIN": {
 		description: "",
+		filter:true,
 		editable: ["LOCAL","NEAR","REMOTE","VIRTUAL","?"],
 		render: {
 			width: 100
@@ -203,6 +229,7 @@ let tableFields = {
 	},
 	"MAPPING.TO": {
 		description: "",
+		filter:true,
 		editable: true,
 		render: {
 			width: 200
@@ -211,6 +238,7 @@ let tableFields = {
 
 	"MAPPING.CONCEPT": {
 		description: "",
+		filter:true,
 		editable: true,
 		render: {
 			width: "auto"
@@ -218,6 +246,7 @@ let tableFields = {
 	},
 	"MAPPING.REPRESENTATION": {
 		description: "",
+		filter:true,
 		editable: true,
 		render: {
 			width: "auto"
@@ -225,6 +254,7 @@ let tableFields = {
 	},
 	"MAPPING.MEANING": {
 		description: "",
+		filter:true,
 		editable: true,
 		render: {
 			width: "auto"
@@ -234,6 +264,7 @@ let tableFields = {
 
 	"MAPPING.LEVEL": {
 		description: "",
+		filter:true,
 		editable: ["ICONIC","INDEXICAL","SYMBOLIC","?"],
 		render: {
 			width: 100
@@ -241,12 +272,14 @@ let tableFields = {
 	},
 	"OUTPUT.ACTIVATION": {
 		editable: ["CONTINUOUS","EVENTBASED","OBJECTBASED","USERBASED","?"],
+		filter:true,
 		render: {
 			width: 125
 		}
 	},
 	"OUTPUT.TRANSITION_TO_FOREGROUND": {
 		description: "",
+		filter:true,
 		editable: ["CHANGE_BLIND","MAKE_AWARE","INTERRUPT","DEMAND_ATTENTION","USER_POLL","?"],
 		render: {
 			width: 125
@@ -254,6 +287,7 @@ let tableFields = {
 	},
 	"OUTPUT.MEDIUM": {
 		description: "",
+		filter:true,
 		editable: ["AMBIENCE","ASSOCIATED_OBJECT","KNOWN_OBJECT","ARTIFICIAL_OBJECT","?"],
 		render: {
 			width: 125
@@ -261,6 +295,8 @@ let tableFields = {
 	},
 	"OUTPUT.MODALITY": {
 		description: "",
+		searchable: true,
+		filter:true,
 		editable: multiple(["AUDIO","VISUAL","HAPTIC","TASTE","SMELL","PAIN","EQUILIBRIO"]),
 		render: {
 			width: 125
@@ -268,6 +304,7 @@ let tableFields = {
 	},
 	"OUTPUT.LOCALIZATION": {
 		description: "",
+		filter:true,
 		editable: ["AMBIENCE","DIRECTIONAL","ROUGHLY","EXACT","?"],
 		render: {
 			width: 125
@@ -275,6 +312,7 @@ let tableFields = {
 	},
 	"OUTPUT.ACCESS": {
 		description: "",
+		filter:true,
 		editable: ["PERSONAL","PARTNERSHIP","FAMILY","GROUP","PUBLIC","?"],
 		render: {
 			width: 125
@@ -282,6 +320,7 @@ let tableFields = {
 	},
 	"OUTPUT.INFORMATION_BANDWIDTH": {
 		description: "",
+		filter:true,
 		editable: ["LOW","MEDIUM","HIGH","?"],
 		render: {
 			width: 100
@@ -289,6 +328,7 @@ let tableFields = {
 	},
 	"EVAL.SAMPLE_SIZE": {
 		description: "",
+		filter:true,
 		editable: true,
 		render: {
 			width: "auto"
@@ -296,6 +336,7 @@ let tableFields = {
 	},
 	"EVAL.TIME_PERIOD": {
 		description: "",
+		filter:true,
 		editable: ["HOURS","WEEKS","MONTHS","?"],
 		render: {
 			width: 100
@@ -303,6 +344,7 @@ let tableFields = {
 	},
 	"EVAL.SETTING": {
 		description: "",
+		filter:true,
 		editable: ["IN_SITU","IN_LAB","IN_PRESENTATION","?"],
 		render: {
 			width: 125
@@ -310,6 +352,7 @@ let tableFields = {
 	},
 	"EVAL.STUDY_GROUP": {
 		description: "",
+		filter:true,
 		editable: ["USERS","EXPERTS","?"],
 		render: {
 			width: 100
@@ -317,6 +360,7 @@ let tableFields = {
 	},
 	"EVAL.DEVSTAGE": {
 		description: "",
+		filter:true,
 		editable: ["PRESENTATION","WOZ","REDUCED_PROTOTYPE","PROTOTYPE","FINAL","?"],
 		render: {
 			width: 200
@@ -324,6 +368,7 @@ let tableFields = {
 	},
 	"EVAL.USABILITY": {
 		description: "",
+		filter:true,
 		editable: multiple(["QUESTIONNAIRE","INTERVIEW","COMMENTS","OBSERVATION","HEURISTIC_EVALUATION","USAGE_STATISTIC","TASK_PERFORMANCE"]),
 		render: {
 			width: 200
@@ -331,6 +376,7 @@ let tableFields = {
 	},
 	"EVAL.COGNITIVE_LOAD": {
 		description: "",
+		filter:true,
 		editable: multiple(["QUESTIONNAIRE","DUAL_TASK","TASK_SHEDDING","PHYSIOLOGICAL (EYE)","PHYSIOLOGICAL (BRAIN)","PHYSIOLOGICAL (HEART)","PHYSIOLOGICAL (RESP)","PHYSIOLOGICAL (SKIN)"]),
 		render: {
 			width: 200
@@ -338,6 +384,7 @@ let tableFields = {
 	},
 	"EVAL.CHANGED_BEHAVIOUR": {
 		description: "",
+		filter:true,
 		editable: multiple(["SUBJECTIVE","OBJECTIVE"]),
 		render: {
 			width: "auto"
