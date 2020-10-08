362e74b7c7490a535157057415115deac43a72bd
Carsten Schwede <cschwede@techfak.uni-bielefeld.de>
2020-10-08 16:49:24 +0200

    Remove LiveReload from index.html and inject it when run locally.

--- tools/devserver.js
+++ tools/devserver.js
@@ -1,4 +1,21 @@
-var connect  = require('connect');
+var lrInject = require('inject-lr-script')
+var stacked = require('stacked')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+var app = stacked()
+app.use(lrInject())
+app.use(serveStatic(__dirname + '/../'))
+
+var server = http.createServer(app)
+server.listen(8080);
+
+var livereload = require('livereload');
+var lrserver = livereload.createServer();
+lrserver.watch(__dirname + "/../");
+
+
+/*var connect  = require('connect');
 var compiler = require('connect-compiler');
 var static = require('serve-static');
 
@@ -9,6 +26,4 @@ server.use(  static(__dirname + '/../'));
 
 server.listen(8080);
 
-var livereload = require('livereload');
-var lrserver = livereload.createServer();
-lrserver.watch(__dirname + "/../");
+*/
